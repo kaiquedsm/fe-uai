2238c55c8f0ee5f94a491862f98807d3562fa8c6
Kaique DSM <kaiquedsm15@gmail.com>
2024-03-17 20:46:26 -0300

    Implementação de todas as estruturas da página home e alguns ajustes no carrossel da primeira parte

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  
+  number_img_carousel: number = 1;
 
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      if(this.number_img_carousel === 4) {
+        this.number_img_carousel = 1
+        return
+      }
+      this.number_img_carousel++
+      console.log(this.number_img_carousel);
+      
+    }, 5000)
   }
 
 }
