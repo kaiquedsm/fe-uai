7d77f5e561c1e59e5ff7964182cdae0b26a9ecf0
Kaique DSM <kaiquedsm15@gmail.com>
2024-03-27 14:19:28 -0300

    Mais algumas modificações na responsividade, com a adição de imagens do carrossel 1 para o desktop

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { NgIfContext } from '@angular/common';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 
 @Component({
@@ -9,17 +10,23 @@ import { initFlowbite } from 'flowbite';
 export class HomeComponent implements OnInit {
   
   number_img_carousel: number = 1;
-
+  screen_width: number = window.innerWidth
+  elseDiv!: TemplateRef<NgIfContext<boolean>>|null;
+  
+  
   constructor() { }
 
   ngOnInit(): void {
+    window.addEventListener('resize', () => {
+      this.screen_width = window.innerWidth;
+   });
     setInterval(() => {
       if(this.number_img_carousel === 4) {
         this.number_img_carousel = 1
         return
       }
       this.number_img_carousel++
-      console.log(this.number_img_carousel);
+      console.log(this.screen_width);
       
     }, 5000)
 
