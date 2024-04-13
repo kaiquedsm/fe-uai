837745ba91536d0a9f4af9b5786b0c8c886722f6
Kaique DSM <kaiquedsm15@gmail.com>
2024-04-13 11:39:09 -0300

    Modificação no primeiro carrossel, apenas o tamanho das imagens precisa ser trocado, só falta a responsividade do segundo carrossel e mínimos detalhes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,13 +13,31 @@ export class HomeComponent implements OnInit {
   number_img_carousel: number = 1;
   screen_width: number = window.innerWidth
   elseDiv!: TemplateRef<NgIfContext<boolean>>|null;  
+  images_1: string[] = ['../../../assets/imgs/carousel_1/1/img_1.svg', '../../../assets/imgs/carousel_1/1/img_2.svg', '../../../assets/imgs/carousel_1/1/img_3.svg', '../../../assets/imgs/carousel_1/1/img_4.svg',]; 
+  images_2: string[] = ['../../../assets/imgs/carousel_1/2/img_1.svg', '../../../assets/imgs/carousel_1/2/img_2.svg', '../../../assets/imgs/carousel_1/2/img_3.svg', '../../../assets/imgs/carousel_1/2/img_4.svg',]; 
+  image_op: string[] = [];
+  currentIndex: number = 0;
   
   constructor() { }
 
   ngOnInit(): void {
+    
+    // this.startCarousel();
+    
     window.addEventListener('resize', () => {
       this.screen_width = window.innerWidth;
    });
+   
+   if(this.screen_width >= 640) {
+    this.image_op = this.images_1
+   } else {
+    this.image_op = this.images_2
+   }
+    
+    setInterval(() => { 
+      this.currentIndex = (this.currentIndex + 1) % this.image_op.length;
+    }, 5000);
+    
     setInterval(() => {
       if(this.number_img_carousel === 4) {
         this.number_img_carousel = 1
@@ -31,8 +49,13 @@ export class HomeComponent implements OnInit {
     }, 5000)
 
     initFlowbite();
+    
   }
-
+  
+  startCarousel(): void {
+     // Alterar a imagem a cada 3 segundos (3000 milissegundos)  
+  }
+    
 
 
 }
