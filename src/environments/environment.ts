c791bfbe5a3051db286c03351d5d834675e9d943
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-04-16 21:44:51 -0300

    build: api target

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  target: 'https://projetomarketing.onrender.com/projetomarketing/v1'
 };
 
 /*
