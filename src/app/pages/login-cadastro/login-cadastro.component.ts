51c18a257a6cfa53e09285beddebda68a6c14d85
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-04-16 21:45:21 -0300

    feat: login method

--- src/app/pages/login-cadastro/login-cadastro.component.ts
+++ src/app/pages/login-cadastro/login-cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-login-cadastro',
@@ -6,8 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-cadastro.component.scss']
 })
 export class LoginCadastroComponent implements OnInit {
-  
-  constructor() { }
+
+  cpfFormControl: FormControl = new FormControl<string>('', [Validators.required]);
+  senhaFormControl: FormControl = new FormControl<string>('', [Validators.required]);
+
+  enviado: boolean = false;
+
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
     const sign_in_btn = document.querySelector<HTMLButtonElement>("#sign-in-btn");
@@ -25,7 +33,7 @@ export class LoginCadastroComponent implements OnInit {
       sign_up_btn.addEventListener("click", () => {
         container.classList.add("sign-up-mode");
       });
-    
+
       sign_in_btn.addEventListener("click", () => {
         container.classList.remove("sign-up-mode");
       });
@@ -57,4 +65,38 @@ export class LoginCadastroComponent implements OnInit {
       inputSenhaCadastro.type = "password";
     });
   }
+
+  login() {
+    this.enviado = true;
+    if (this.cpfFormControl.valid && this.senhaFormControl.valid) {
+      const dadosLogin = {
+        cpf: this.cpfFormControl.value,
+        senha: this.senhaFormControl.value
+      };
+      this.userService.autenticar(dadosLogin).subscribe({
+        next: (response) => {
+          localStorage.setItem('dadosLogin', JSON.stringify(response)); // Salvei os dados do login no localStorage.
+          this.userService.dadosLogin.next(response);
+          this.router.navigate(['/']); // Aqui eu redireciono o usuário de volta pra home se deu certo. Quando a próxima tela depois do login estiver pronta, deve ser redirecionado pra lá.
+        },
+        error: (err:
+          {
+            error: {
+              message: string,
+              uri?: string,
+              body?: {
+                senha?: string,
+                cpf?: string
+              }
+            }
+          }) => {
+          console.log(err.error.message);
+          console.log(err.error.uri ?? err.error.body?.senha ?? err.error.body?.cpf);
+        }
+      })
+    }
+
+
+  }
+
 }
