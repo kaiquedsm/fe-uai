438347b802569f90c4f2ca696ad9233dabdf74da
Joao Gobi <joao_gobi2012@hotmail.com>
2024-04-20 11:08:51 -0300

    feat: rota para o chat adicionada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginCadastroComponent } from './pages/login-cadastro/login-cadastro.component';
+import { ChatComponent } from './pages/chat/chat.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'login-cadastro', component: LoginCadastroComponent}
+  {path: 'login-cadastro', component: LoginCadastroComponent},
+  {path: 'chat', component: ChatComponent}
 ];
 
 @NgModule({
