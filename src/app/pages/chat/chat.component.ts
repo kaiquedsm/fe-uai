2f389137fb94ce716359ee83dc642d1de9f24621
Joao Gobi <joao_gobi2012@hotmail.com>
2024-04-21 20:33:18 -0300

    feat: implementaçao da tela de chat sem responsividade

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
+  topicos = [
+    {},
+    {},
+    {},
+    {},
+    {},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
