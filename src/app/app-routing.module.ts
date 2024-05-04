347615e3f545467682623598bc89a1b7196c73b4
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-05-03 21:40:16 -0300

    feat: hashStrategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, provideRouter } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginCadastroComponent } from './pages/login-cadastro/login-cadastro.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'login-cadastro', component: LoginCadastroComponent}
+  { path: '', component: HomeComponent },
+  { path: 'login-cadastro', component: LoginCadastroComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
